1e2a8322335c43bf4494004602dd58c81931f349
NagimovMV <139386381+NagimovMV@users.noreply.github.com>
2023-07-13 00:35:04 +0400

    initial commit

---
+++ GUI_START.bat
@@ -0,0 +1,3 @@
+cd ./bin/
+java -Xms500m -Xmx1g -XX:+UseG1GC -XX:G1HeapRegionSize=2m -XX:InitiatingHeapOccupancyPercent=65 -jar ApacheJMeter.jar -t ../Slotegrator_NagimovMV_120723.jmx -l ../RAWRESULT.jtl -f
+pause
